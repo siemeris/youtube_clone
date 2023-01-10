a62ba91c2458ddb96d72a3cfc3210a87cbc21248
siemeris <siemeris@gmail.com>
2023-01-10 16:48:35 +0000

    routes and components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Box } from '@mui/material'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+
+const App = () => (
+  <BrowserRouter>
+  <Box sx={{backgroundColor: '#000'}}>
+  <Navbar />
+  <Routes>
+    <Route path="/" exact element={<Feed />} />
+    <Route path="/video/:id" exact element={<VideoDetail />} />
+    <Route path="/channel/:id" exact element={<ChannelDetail />} />
+    <Route path="/search/:searchTerm" exact element={<SearchFeed />} />
+  </Routes>
+  </Box>
+  </BrowserRouter>
+)
 
 export default App
