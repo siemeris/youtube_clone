b339d91a347a0686550c1b5c05551e04f5d09fc6
siemeris <siemeris@gmail.com>
2023-01-10 17:08:46 +0000

    improve import components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React from 'react'
 import { Box } from '@mui/material'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
+// import Navbar from './components/Navbar'
+// import Feed from './components/Feed'
+// import VideoDetail from './components/VideoDetail'
+// import ChannelDetail from './components/ChannelDetail'
+// import SearchFeed from './components/SearchFeed'
+// Para evitar tener cinco líneas importando 5 componentes creamos el index.js en la carpeta componentes
+import {Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed} from '../src/components'
 
 const App = () => (
   <BrowserRouter>
